05ac0a53a01e1dbb4c55a75d3001dd14eb4eae94
OkkarMin <OMIN001@e.ntu.edu.sg>
2022-03-29 21:11:51 +0800

    chore: test express app update duration

--- express/src/app.ts
+++ express/src/app.ts
@@ -9,7 +9,7 @@ app.get("/health", (req: Request, res: Response) => {
 });
 
 app.get("/newRoute1", (req: Request, res: Response) => {
-  res.send("hello from newRoute1");
+  res.send("hello from newRoute1 with changed message");
 });
 
 app.get("*", (req: Request, res: Response) => {
